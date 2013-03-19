4add58d9f941bb416bac6b4d15c776e4fbe8c0da
Kaitlin Lee <katycorp@gmail.com>
2013-03-18 21:34:09 -0400

    added saving to transactional md data

--- scrapers/md/transactional_expenditures.py
+++ scrapers/md/transactional_expenditures.py
@@ -1,4 +1,4 @@
-from billy.scrape.expenditures import TransactionalExpenditureScraper, AggregatedExpenditureScraper
+from billy.scrape.expenditures import TransactionalExpenditureScraper, AggregatedExpenditureScraper, TransactionalExpenditure, AggregatedExpenditure
 from collections import defaultdict
 import lxml.html
 import xlrd
@@ -30,17 +30,31 @@ class MDTransactionalExpenditureScraper(TransactionalExpenditureScraper):
 
             row_data = sheet.row(row_num)
 
-            record = {}
-            record['agency_name'], record['subagency_name'] = self.split_agency(sheet.cell_value(row_num, 0))
-            record['recipient_name'] = sheet.cell_value(row_num, 1)
-            record['recipient_zip'] = sheet.cell_value(row_num, 2)
-            record['fiscal_year'] = sheet.cell_value(row_num, 3)
-            record['amount'] = float(sheet.cell_value(row_num, 4) or 0) 
-            record['description'] = sheet.cell_value(row_num, 5)
-            record['spending_type'] = sheet.cell_value(row_num, 6).lower()
+            fiscal_year = None
+            amount = None
+            recipient_name = None
+            spending_type = None
+    
+            if sheet.cell_value(row_num, 1) and sheet.cell_value(row_num, 3) and sheet.cell_value(row_num, 4) and sheet.cell_value(row_num, 6) :
+                fiscal_year = sheet.cell_value(row_num, 3)
+                amount = float(sheet.cell_value(row_num, 4)) 
+                recipient_name = sheet.cell_value(row_num, 1)
+                spending_type = sheet.cell_value(row_num, 6).lower()
+            else:
+                continue
 
-            print record
-                
+            record = {}
+            if sheet.cell_value(row_num, 0):
+                record['agency_name'], record['subagency_name'] = self.split_agency(sheet.cell_value(row_num, 0))
+            if sheet.cell_value(row_num, 2):
+                record['recipient_zip'] = sheet.cell_value(row_num, 2)
+            if sheet.cell_value(row_num, 5):
+                record['description'] = sheet.cell_value(row_num, 5)
+
+#            print record
+            exp = TransactionalExpenditure(fiscal_year, spending_type, recipient_name, amount, **record)
+            exp.add_source(url)
+            self.save_object(exp)
 
     def scrape_payments(self):
         url = 'http://spending.dbm.maryland.gov/'
@@ -50,3 +64,7 @@ class MDTransactionalExpenditureScraper(TransactionalExpenditureScraper):
 
         self.scrape_grants()
         self.scrape_payments()
+
+
+
+
